59d805b814295c3459708d09aeb11c282e80ba42
MayyDayy99 <pr.nemes@gmail.com>
2026-03-28 13:53:24 +0100

    use custom firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,17 +2,16 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut } from 'firebase/auth';
 import { getFirestore, collection, doc, setDoc, getDoc, getDocs, query, where, onSnapshot, addDoc, serverTimestamp, getDocFromServer } from 'firebase/firestore';
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyC8E5q9agK_utY_GZzvt9NIvIO0b1HV2Gk",
+  authDomain: "cinepair-31543.firebaseapp.com",
+  projectId: "cinepair-31543",
+  storageBucket: "cinepair-31543.firebasestorage.app",
+  messagingSenderId: "113009325170",
+  appId: "1:113009325170:web:76cb08f667a371ddd8feeb"
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, import.meta.env.VITE_FIREBASE_FIRESTORE_DB_ID || '(default)');
+export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 
